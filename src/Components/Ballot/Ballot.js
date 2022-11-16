2ec88cc34b80e9518d759a28cc0386b46ad55aa1
marurevi <maryrev@hotmail.com>
2022-11-15 21:52:10 -0300

    Fetch api elements and render them in the component

--- src/Components/Ballot/Ballot.js
+++ src/Components/Ballot/Ballot.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React,{useEffect, useState} from 'react';
 
 const Ballot = () => {
+  const [ballot, setBallot] = useState([]);
+  useEffect(() => {apiCall()},[])
+  const apiCall = async() => {
+    await fetch('/api/getBallotData').then(res => {
+      return res.json();
+    }).then(data => { setBallot(data.items) });
+  }
+  console.log('ballot', ballot);
+  
   return (
     <div className='ballot'>
-      'Your Code Goes Here'
+      {ballot && ballot.map((item) => {
+        return (
+          <div key={item.id}>
+            <h2>Category: {item.title}</h2>
+            {item.items.map((el)=>{
+              return(
+                <ul key={el.id}>
+                  <li><h3>{el.title}</h3></li>
+                  <li><img src={el.photoUrL} alt={el.title}/></li>
+                </ul>
+              )
+            })}
+          </div>
+        )
+      })}
     </div>
   )
 }
